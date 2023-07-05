779613b4f31ee5ffc787be31c3480effe98f25e8
Farai Zihove <farai@flickerleap.com>
2023-07-05 17:06:36 +0200

    add swagger

--- src/assessment/app/Http/Controllers/AreaController.php
+++ src/assessment/app/Http/Controllers/AreaController.php
@@ -10,6 +10,31 @@ use Illuminate\Http\JsonResponse;
 
 class AreaController extends Controller
 {
+    /**
+     * @OA\Get(
+     *    path="/areas",
+     *    operationId="index",
+     *    tags={"Areas"},
+     *    summary="Get list of areas",
+     *    description="Get list of areas",
+     *    @OA\Parameter(name="limit", in="query", description="limit", required=false,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *    @OA\Parameter(name="page", in="query", description="the page number", required=false,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *    @OA\Parameter(name="order", in="query", description="order  accepts 'asc' or 'desc'", required=false,
+     *        @OA\Schema(type="string")
+     *    ),
+     *     @OA\Response(
+     *          response=200, description="Success",
+     *          @OA\JsonContent(
+     *             @OA\Property(property="status", type="integer", example="200"),
+     *             @OA\Property(property="data",type="object")
+     *          )
+     *       )
+     *  )
+     */
 
     public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
@@ -17,6 +42,29 @@ class AreaController extends Controller
     }
 
 
+    /**
+     * @OA\Post(
+     *      path="/areas",
+     *      operationId="store",
+     *      tags={"Areas"},
+     *      summary="Store Area in DB",
+     *      description="Store Area in DB",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *            required={"area_name"},
+     *            @OA\Property(property="area_name", type="string", format="string", example="Harare CBD")
+     *         ),
+     *      ),
+     *     @OA\Response(
+     *          response=200, description="Success",
+     *          @OA\JsonContent(
+     *             @OA\Property(property="status", type="integer", example=""),
+     *             @OA\Property(property="data",type="object")
+     *          )
+     *       )
+     *  )
+     */
     public function store(StoreAreaRequest $request)
     {
         $createdArea = Area::query()->create([
--- src/assessment/app/Http/Controllers/Controller.php
+++ src/assessment/app/Http/Controllers/Controller.php
@@ -7,6 +7,25 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Backend Developemnt Assessment Api Documentation",
+ *     description="Econet Backend Development Assessment Api Documentation",
+ *     @OA\Contact(
+ *         name="Farai Zihove",
+ *         email="zihovem@gmail.com@"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * ),
+ * @OA\Server(
+ *     url="/api",
+ * ),
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
